85f2c78905d6e8566b68ed87398c2a1ee07228de
Akinwalire Olanrewaju <olanrewajuakinwalire@gmail.com>
2024-11-26 13:14:42 +0100

    Latest Update

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 const hamburger = document.getElementById("hamburgerMenu");
 const oneSide = document.getElementById("oneSide");
 const signUp = document.getElementById("signUp");
-
+const burGer = document.querySelector(".burger");
 hamburger.addEventListener("click", () => {
-  oneSide.classList.toggle("active");
+  // oneSide.classList.toggle("active");
+
+  // Another method to do it
+
+  // if (oneSide.classList.contains("active")) {
+  //   console.log("Active class removed");
+  //   oneSide.classList.remove("active");
+  // } else {
+  //   console.log("Active class added");
+  //   oneSide.classList.add("active");
+  // }
+
+  // Another Method
+
+  if (!oneSide.classList.contains("active")) {
+    oneSide.classList.add("active");
+    burGer.classList.replace("ti-menu-2", "ti-x");
+    // console.log("Active class added");
+  } else {
+    oneSide.classList.remove("active");
+    burGer.classList.replace("ti-x", "ti-menu-2");
+    // console.log("Active class removed");
+  }
+
   signUp.classList.remove("btn-dark");
 });
 
